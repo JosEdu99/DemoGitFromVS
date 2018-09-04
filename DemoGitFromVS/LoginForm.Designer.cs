c0efad518071c85508ded3a7d5ecd41f4bcaebaf
Eduardo <josedu_uc30@hotmail.com>
2018-09-04 11:54:57 -0600

    Formulario de inicio de sesión

---
+++ DemoGitFromVS/LoginForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DemoGitFromVS
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.Label();
+            this.txtboxUsuario = new System.Windows.Forms.TextBox();
+            this.txtboxContraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(72, 67);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            this.lblUsuario.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.AutoSize = true;
+            this.txtContraseña.Location = new System.Drawing.Point(72, 170);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(61, 13);
+            this.txtContraseña.TabIndex = 1;
+            this.txtContraseña.Text = "Contraseña";
+            // 
+            // txtboxUsuario
+            // 
+            this.txtboxUsuario.Location = new System.Drawing.Point(75, 98);
+            this.txtboxUsuario.Name = "txtboxUsuario";
+            this.txtboxUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtboxUsuario.TabIndex = 2;
+            // 
+            // txtboxContraseña
+            // 
+            this.txtboxContraseña.Location = new System.Drawing.Point(75, 206);
+            this.txtboxContraseña.Name = "txtboxContraseña";
+            this.txtboxContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtboxContraseña.TabIndex = 3;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtboxContraseña);
+            this.Controls.Add(this.txtboxUsuario);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label txtContraseña;
+        private System.Windows.Forms.TextBox txtboxUsuario;
+        private System.Windows.Forms.TextBox txtboxContraseña;
+    }
+}
